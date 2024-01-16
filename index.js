86c28d67f4c69f58b574e37f837aa4bbc29fed12
Felix Ouma <moxiemeyoh@gmail.com>
2024-01-16 12:58:22 +0300

    Connection to MongoDB established

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
-
+//const dotenv = require('dotenv');
+//dotenv.config({ path: './config.env' });
+const mongoose = require('mongoose');
 
 // initializing the app
 const app = express();
+const port = 8000;
+DATABASE_CONNECT = "mongodb://127.0.0.1:27017/ICanHelpFoundation";
 
+// connect to databse
+const connectDB = async () => {
+    try {
+        await mongoose.connect( DATABASE_CONNECT, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("DB connection is Successful");
+    } catch (error) {
+        console.error(error.message);
+    }
+}
 
 // basic routing
 app.get('/', (req, res) => {
@@ -11,7 +27,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = 8000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    connectDB();
 });

61014d5f8f9b614e5d65bf520bab035bfe220212
Felix Ouma <moxiemeyoh@gmail.com>
2024-02-04 11:17:40 +0300

    Add: dotenv packages  -configure dotenv  - access env variables

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const usersRouter = require("./routes/userRoute");
 const dotenv = require('dotenv');
+const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 dotenv.config({ path: './config.env' });
 
@@ -11,7 +13,23 @@ const port = process.env.PORT;
 
 // middlewares
 app.use(express.json({limit:'100kb'}));
-
+app.use(cors());
+app.use(
+        '/',
+        createProxyMiddleware({
+            target: 'http://localhost:8000',
+            changeOrigin: true,
+        })
+    );
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    next();
+});
 // routings
 app.use('/api/v1/users', usersRouter);
 
